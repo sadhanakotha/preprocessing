bc50ae0310c59196e346058acce8d3f5908e50dc
sadhanakotha <kotha.sadhana@gmail.com>
2024-02-27 13:23:31 -0500

    Added preprocessing logic by accepting input file and writing it to output file.

---
+++ src/preprocessing.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class preprocessing {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("resources/input.txt");
+        File output_file = new File("resources/output.txt");
+        BufferedReader bufRdr = new BufferedReader(new FileReader(file));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(output_file));
+        String line = null;
+
+        try {
+            while ((line = bufRdr.readLine()) != null) {
+                // read line by line and append some string to the line
+                writer.write(line + " checked.");
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            // ...
+            //
+        }
+        writer.close();
+        bufRdr.close();
+
+    }
+}
